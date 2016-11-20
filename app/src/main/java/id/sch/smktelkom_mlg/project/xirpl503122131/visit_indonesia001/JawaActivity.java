6901aebda2fcd9deeb066676cfb35a716c7bc398
USER-ASUS <Alfancahyo11@gmail.com>
2016-11-20 10:48:45 +0700

    * Act * Papua Jawa

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/JawaActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/JawaActivity.java
@@ -1,13 +1,37 @@
 package id.sch.smktelkom_mlg.project.xirpl503122131.visit_indonesia001;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class JawaActivity extends AppCompatActivity {
+    Button btj1, btj2, btj3, btj4, btj5, btj6, btj7, btj8, btj9, btj10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jawa);
+
+        setTitle("Jawa");
+
+        btj1 = (Button) findViewById(R.id.buttonJ1);
+        btj2 = (Button) findViewById(R.id.buttonJ2);
+        btj3 = (Button) findViewById(R.id.buttonJ3);
+        btj4 = (Button) findViewById(R.id.buttonJ4);
+        btj5 = (Button) findViewById(R.id.buttonJ5);
+        btj6 = (Button) findViewById(R.id.buttonJ6);
+        btj7 = (Button) findViewById(R.id.buttonJ7);
+        btj8 = (Button) findViewById(R.id.buttonJ8);
+        btj9 = (Button) findViewById(R.id.buttonJ9);
+        btj10 = (Button) findViewById(R.id.buttonJ10);
+
+        btj1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(JawaActivity.this, DetailActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/PapuaActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/PapuaActivity.java
@@ -1,13 +1,37 @@
 package id.sch.smktelkom_mlg.project.xirpl503122131.visit_indonesia001;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class PapuaActivity extends AppCompatActivity {
+    Button btp1, btp2, btp3, btp4, btp5, btp6, btp7, btp8, btp9, btp10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_papua);
+
+        setTitle("Papua");
+
+        btp1 = (Button) findViewById(R.id.buttonP1);
+        btp2 = (Button) findViewById(R.id.buttonP2);
+        btp3 = (Button) findViewById(R.id.buttonP3);
+        btp4 = (Button) findViewById(R.id.buttonP4);
+        btp5 = (Button) findViewById(R.id.buttonP5);
+        btp6 = (Button) findViewById(R.id.buttonP6);
+        btp7 = (Button) findViewById(R.id.buttonP7);
+        btp8 = (Button) findViewById(R.id.buttonP8);
+        btp9 = (Button) findViewById(R.id.buttonP9);
+        btp10 = (Button) findViewById(R.id.buttonP10);
+
+        btp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PapuaActivity.this, DetailActivity.class));
+            }
+        });
     }
 }
