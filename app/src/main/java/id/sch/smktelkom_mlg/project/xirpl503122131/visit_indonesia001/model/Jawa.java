7a62b6165f9bec8b5777712512e8629e6168ff09
septy <septydarmantya@gmail.com>
2016-12-04 09:39:37 +0700

    Jawa Model

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/model/Jawa.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/model/Jawa.java
@@ -1,4 +1,19 @@
 package id.sch.smktelkom_mlg.project.xirpl503122131.visit_indonesia001.model;
 
-public class Jawa {
+import java.io.Serializable;
+
+public class Jawa implements Serializable {
+    public String judul;
+    public String deskripsi;
+    public String detail;
+    public String lokasi;
+    public String foto;
+
+    public Jawa(String judul, String deskripsi, String detail, String lokasi, String foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.detail = detail;
+        this.lokasi = lokasi;
+        this.foto = foto;
+    }
 }
