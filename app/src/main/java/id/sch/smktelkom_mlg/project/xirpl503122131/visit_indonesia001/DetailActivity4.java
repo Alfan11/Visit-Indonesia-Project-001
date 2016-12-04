de1953844381ad565153393f25e2362fd580efcf
USER-ASUS <Alfancahyo11@gmail.com>
2016-12-04 13:08:16 +0700

    DetailActivity 3 4

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/DetailActivity4.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/DetailActivity4.java
@@ -1,7 +1,39 @@
 package id.sch.smktelkom_mlg.project.xirpl503122131.visit_indonesia001;
 
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import id.sch.smktelkom_mlg.project.xirpl503122131.visit_indonesia001.model.Papua;
 
 public class DetailActivity4 extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Papua hotel = (Papua) getIntent().getSerializableExtra(PapuaActivity.HOTEL);
+        setTitle(hotel.judul);
+        ImageView ivFoto = (ImageView) findViewById(R.id.imageFoto);
+        ivFoto.setImageURI(Uri.parse(hotel.foto));
+        TextView tvDeskripsi = (TextView) findViewById(R.id.place_detail);
+        tvDeskripsi.setText(hotel.deskripsi + "\n\n" + hotel.detail);
+        TextView tvLokasi = (TextView) findViewById(R.id.place_location);
+        tvLokasi.setText(hotel.lokasi);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
 }
