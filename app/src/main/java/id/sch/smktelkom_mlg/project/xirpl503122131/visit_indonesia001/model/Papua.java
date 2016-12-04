e169aaa7da341a67fbc6f262040b9f841638ffbd
GABS <galbie_elinour_24rpl@student.smktelkom-mlg.sch.id>
2016-12-04 10:07:09 +0700

    Papua Model

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/model/Papua.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl503122131/visit_indonesia001/model/Papua.java
@@ -3,5 +3,17 @@ package id.sch.smktelkom_mlg.project.xirpl503122131.visit_indonesia001.model;
 import java.io.Serializable;
 
 public class Papua implements Serializable {
+    public String judul;
+    public String deskripsi;
+    public String detail;
+    public String lokasi;
+    public String foto;
 
+    public Papua(String judul, String deskripsi, String detail, String lokasi, String foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.detail = detail;
+        this.lokasi = lokasi;
+        this.foto = foto;
+    }
 }
